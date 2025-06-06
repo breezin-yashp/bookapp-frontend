08e3ebfddf86155dd92296f2d3401d61a459e342
Yash Priyadarshi <yashp@sprinto.com>
2025-06-06 18:38:54 +0530

    fixed env

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql'
+  uri: 'https://bookapp-backend-uhxp.onrender.com'
 });
 
 const client = new ApolloClient({
